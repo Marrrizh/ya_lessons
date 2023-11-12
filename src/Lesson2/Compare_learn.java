262b072b9028fcd39d8ccb38275d6eb3943b1778
Masha <mariz87@yandex.ru>
2023-11-12 21:36:38 +0300

    задача на сравнение

---
+++ src/Lesson2/Compare_learn.java
@@ -0,0 +1,19 @@
+package Lesson2;
+
+public class Compare_learn {
+    public static void main(String[] args) {
+        int answer1 = 3; // Ответ на 1 вопрос
+        int answer2 = 4; // Ответ на 2 вопрос
+        int answer3 = 1; // Ответ на 3 вопрос
+
+        System.out.println("Результат проверки вопроса 1:");
+        // Выведите true или false, сравнив ответ на 1 вопрос с правильным
+        System.out.println(answer1 == 2);
+        System.out.println("Результат проверки вопроса 2:");
+        System.out.println(answer2 != 4);
+        System.out.println("Результат проверки вопроса 3:");
+        System.out.println(answer3 == 1);
+        // Повторите для вопросов 2 и 3
+    }
+}
+
